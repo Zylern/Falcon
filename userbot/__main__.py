c716202edbbf3e490dc4066c06acf830aa8d1af2
Zylern <47510180+Zylern@users.noreply.github.com>
2020-08-20 08:56:26 +0000

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -45,7 +45,7 @@ async def a():
      import_module(f"userbot.modules.{cr}")
      la += 1
      LOGS.info(f" loaded {la}/{f} modules")  
-  os.system("rm userbot/modules/*.py") ; LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !falcon in any client's chat, type  !help for more info.")
+  os.system("rm userbot/modules/*.py") ; LOGS.info(f"WEW your userbot i active now with {o}{o2}{o3}{o4} check it by typing .alive in any chat, type  .help for more info.")
   if len(argv) not in (1, 3, 4):
        await falcon.disconnect()
   else:
